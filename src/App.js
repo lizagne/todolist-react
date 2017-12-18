dda61c4c8abe2ce9e8d61f83043d0e00fc7995d3
Liz Verde <>
2017-12-18 21:27:12 +0000

    Adding the AddButton to the render

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './App.css';
 import TaskList from './components/TaskList';
 import DateComp from './components/DateComp';
-import Avatar from './components/Avatar'
+import Avatar from './components/Avatar';
+import AddButton from './components/AddButton';
 
 const App = () => (
-  	<div>
+  	<div style={{ padding: '30px 30px' }}>
   		<Avatar />
     	<DateComp />
     	<br />
     	<TaskList />
+    	<br />
+		<AddButton />
     </div>	
 )
 
