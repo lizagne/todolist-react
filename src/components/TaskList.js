a0f8315ac0ed78ba52b79505a8246897dd90938f
Liz Verde <>
2017-12-18 21:50:50 +0000

    Adding props to the Task component and information to the TaskList component and also changing them into class components so that they can accept props.

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,16 +1,20 @@
 //This is the parent of Task.js and the child of App.js
-import React from 'react';
+import React, { Component } from 'react';
 import Task from './Task';
 
-const TaskList = () => (
+class TaskList extends Component {
+	render() {
+		return (	
+			<div>
+				<Task time="12" period="AM" activity_title="Finish Tutorial Series" activity_description="#ReactForNewbies" />
+				<Task time="9" period="AM" activity_title="Meeting with Team Leads" activity_description="New Project Kickoff" />
+				<Task time="11" period="AM" activity_title="Call Mom" activity_description="Return her call before she kills me" />
+				<Task time="3" period="PM" activity_title="Fix Wifey's website" activity_description="FB Ads Integration not working" />
+				<Task time="6" period="PM" activity_title="Do DB Backups" activity_description="Related to upcoming server migration" />
+			</div>
+		)
 
-	<div>
-    	<Task />
-		<Task />
-		<Task />
-		<Task />
-		<Task />
-	</div>
-);
+	}
+};
 
 export default TaskList;
